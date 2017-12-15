5589d863c5c47b9cfcfb6407863493237c31e4aa
백기훈 <masinogns@gmail.com>
2017-12-16 04:37:19 +0900

    #B4

---
+++ js/home.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#jeju").mouseover(function(){
+        $("#jeju-map").css("display", "block");
+    });
+    $("#jeju").mouseout(function(){
+        $("#jeju-map").css("display", "none");
+    });
+});
